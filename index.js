c52ac2bec333df35a0de722fa14096f293aa7882
Sami <sfouad@gmail.com>
2022-06-11 22:38:18 -0600

    tweaks

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const https = require('https');
 const fs = require('fs');
 const axios = require('axios');
+const { execSync } = require('child_process');
 
+// this is for caching all objects in the blockchain data store
 if (process.argv[2] == "--task" && process.argv[3] == "objectGetAll") {
   const data = JSON.stringify({
+    // build request for graphql
     query: `{
               objectGetAll {
                   id
@@ -16,6 +19,7 @@ if (process.argv[2] == "--task" && process.argv[3] == "objectGetAll") {
             }`,
   });
 
+  // https options
   const options = {
     hostname: 'graphql.officer.watch',
     path: '/graphql',
@@ -27,6 +31,8 @@ if (process.argv[2] == "--task" && process.argv[3] == "objectGetAll") {
     },
   };
 
+  // send request
+  // TODO: convert to axois so it can refactored to use async/await
   const req = https.request(options, (res) => {
     let data = '';
     res.on('data', (d) => { data += d; });
@@ -40,14 +46,17 @@ if (process.argv[2] == "--task" && process.argv[3] == "objectGetAll") {
       }
     });
   });
-
   req.on('error', (error) => { console.error(error); });
   req.write(data);
   req.end();
 }
 
+// this is for caching all ipfs json
+// TODO: also query for objType and filter
+// this currently will error out hard if ipfsHash points to media data
 if (process.argv[2] == "--task" && process.argv[3] == "objectGetIpfs") {
   const data = JSON.stringify({
+    // build query for graphql
     query: `{
               objectGetAll {
                   id
@@ -69,6 +78,9 @@ if (process.argv[2] == "--task" && process.argv[3] == "objectGetIpfs") {
 
 
   var cacheArray = {};
+
+  // send request
+  // TODO: convert to axois so it can refactored to use async/await
   const req = https.request(options, (res) => {
     let data = '';
     res.on('data', (d) => { data += d; });
@@ -78,15 +90,35 @@ if (process.argv[2] == "--task" && process.argv[3] == "objectGetIpfs") {
           for (const obj of returnData.data.objectGetAll)
           {
             cacheArray[obj.id] = {};
+            // fetch data at the ipfs hash
             let ipfsData = await axios.get("https://ipfs.officer.watch/ipfs/" + obj.ipfsHash)
                                       .catch(function (err) {
-                                        console.log("Unable to fetch -", err);
+                                        console.error("Unable to fetch -", err);
                                       });
+            // add fetched data to generated json object
             cacheArray[obj.id] = ipfsData.data;
           }
+          // write generated json to file
           fs.writeFileSync('ipfsCache.json', 
           JSON.stringify(cacheArray));
-          //console.log(cacheArray);
+
+          // add generated file to ipfs
+          /*
+          try {
+            let options = {stdio : 'pipe' };
+            let ipfsExec = execSync('ipfs add ipfsCache.json', options);
+            let newIpfsHash = execSync('ipfs add ipfsCache.json -q --only-hash');
+            newIpfsHash = newIpfsHash.toString().trim();
+            let mirrorExec = execSync('ipfs pin remote add --service=pinata --name=ipfsCache.json '+ newIpfsHash);
+          } catch (ierr) {
+            console.error("Unable to add to ipfs -", err);
+          }
+          */
+
+          // TODO: add generated file to blockchain
+          //
+          // code goes here
+          //
       } catch (err) {
           console.error(err);
       }
